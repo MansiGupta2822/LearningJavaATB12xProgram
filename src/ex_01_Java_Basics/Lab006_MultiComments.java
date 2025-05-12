af6268da9fb627896b193621434987901f28fd84
MansiGupta2822 <contactmansigupta2@gmail.com>
2025-05-12 18:57:14 +0530

    ex_01_Java_Basics

---
+++ src/ex_01_Java_Basics/Lab006_MultiComments.java
@@ -0,0 +1,21 @@
+package ex_01_Java_Basics;
+
+public class Lab006_MultiComments {
+
+    /**
+     * Author : Mansi Gupta
+     * Course :  ATB12x
+     * Learning :  Java
+     *
+     **/
+
+    public static void main(String[] args) {
+        /**
+         * Author : Mansi
+         * Course :  ATB12x
+         * Learning :  Java
+         *
+         **/
+        System.out.println("Multi Comment");
+    }
+}
