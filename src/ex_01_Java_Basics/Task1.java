d4b929301cb9236d6a1ff7c024da9712a3589c02
MansiGupta2822 <contactmansigupta2@gmail.com>
2025-05-14 10:31:30 +0530

    ex_01_Java_Basics

---
+++ src/ex_01_Java_Basics/Task1.java
@@ -0,0 +1,20 @@
+package ex_01_Java_Basics;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+
+        int num=5;
+        System.out.printf("%d * 1 =5%n",num);
+        System.out.printf("%d * 2 =10%n",num);
+        System.out.printf("%d * 3 =15%n",num);
+        System.out.printf("%d * 4 =20%n",num);
+        System.out.printf("%d * 5 =25%n",num);
+        System.out.printf("%d * 6 =30%n",num);
+        System.out.printf("%d * 7 =35%n",num);
+        System.out.printf("%d * 8 =40%n",num);
+        System.out.printf("%d * 9 =45%n",num);
+        System.out.printf("%d * 10 =50%n",num);
+
+    }
+}
