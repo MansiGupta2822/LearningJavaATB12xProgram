512cd6899f936e6ae410a0602c72fb8db6c6acaf
MansiGupta2822 <contactmansigupta2@gmail.com>
2025-06-21 13:20:08 +0530

    Question13

---
+++ src/Test/Question13.java
@@ -0,0 +1,16 @@
+package Test;
+
+public class Question13 {
+    public static void main(String[] args) {
+        //Write a program to print multiplication tables using nested loops.
+
+        for(int i=1; i<=5; i++)
+        {
+            System.out.println("Table of:"+ i);
+            for(int j=1; j<=10; j++)
+            {
+                System.out.println(i+"*"+j+"="+i*j);
+            }
+        }
+    }
+}
