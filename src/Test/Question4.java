31a2f009ae5d259fd9cb98b885749b48906d41b4
MansiGupta2822 <contactmansigupta2@gmail.com>
2025-06-21 11:53:55 +0530

    Question4

---
+++ src/Test/Question4.java
@@ -0,0 +1,14 @@
+package Test;
+
+public class Question4 {
+    public static void main(String[] args) {
+        //Create a program that demonstrates pre-increment, post-increment, pre-decrement, and post-decrement operators.
+
+        int i=5;
+        System.out.println("Pre-Incremented value is:" + ++i);//6
+        System.out.println("Post-Incremented value is:" + i++); //6
+        System.out.println("Pre-Decremented value is:" + --i);//6
+        System.out.println("Pre-Decremented value is:" + i--);//6
+
+    }
+}
