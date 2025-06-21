ee0dffafdb2ca348d2c18b8f7aaf34af8b839a33
MansiGupta2822 <contactmansigupta2@gmail.com>
2025-06-21 11:39:09 +0530

    Question2

---
+++ src/Test/question2.java
@@ -0,0 +1,16 @@
+package Test;
+
+public class question2 {
+    public static void main(String[] args) {
+        //Create a Java program that demonstrates various arithmetic and assignment operators.
+
+        int a=10;
+        int b=5;
+
+        System.out.println("Addition:" + (a+b));
+        System.out.println("Substraction:" + (a-b));
+        System.out.println("Multiplication:" + (a*b));
+        System.out.println("Division"+ (a/b));
+        System.out.println("Modulus"+ (a%b));
+    }
+}
